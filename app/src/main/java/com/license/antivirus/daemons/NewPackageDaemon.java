78696e03a6fb1b4c74d3c13dfa93458a41c30a9d
RiZZeR99 <ramusteata@gmail.com>
2022-06-23 14:23:33 +0300

    Cleaned up code.

--- app/src/main/java/com/license/antivirus/daemons/NewPackageDaemon.java
+++ app/src/main/java/com/license/antivirus/daemons/NewPackageDaemon.java
@@ -1,19 +1,13 @@
 package com.license.antivirus.daemons;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 
 import com.license.antivirus.broadcasts.NewPackageBroadcast;
 
@@ -37,7 +31,6 @@ public class NewPackageDaemon extends Service {
     @Override
     public int onStartCommand(Intent intent, int flag, int startId) {
         super.onStartCommand(intent, flag, startId);
-        Context context = this;
         return Service.START_STICKY;
     }
 
@@ -52,18 +45,6 @@ public class NewPackageDaemon extends Service {
         } catch (Exception e) {
             System.out.println("Already register receiver.");
         }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private String createNotificationChannel(NotificationManager notificationManager) {
-        String channelId = "my_service_channelid";
-        String channelName = "My Foreground Service";
-        NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-        // omitted the LED color
-        channel.setImportance(NotificationManager.IMPORTANCE_NONE);
-        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-        notificationManager.createNotificationChannel(channel);
-        return channelId;
     }
 
     @Override
